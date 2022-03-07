9f6a6d07fc6381c1e945833234c52996ded01aa8
Ahmad <ahmadrashid2@gmail.com>
2022-03-06 23:06:13 -0800

    FirstCommit

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,12 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i <3 ; i++) {
+            System.out.println("Printing three times");
+            Thread.sleep(1000);
+        }
+    }
+}
